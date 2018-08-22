28c27512b329580acd68a5ad583f97ad5a77280e
Sofia Estevez <sve6@miami.edu>
2018-08-22 13:21:47 -0400

    Setting up first homework submission on 8/22.

---
+++ week1/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(800, 400);
+  background(204);
+}
+
+function draw() {
+  ellipse(400, 85, 100, 90);
+  ellipse(400, 230, 200, 200);
+  ellipse(380, 70, 20, 20);
+  ellipse(420, 70, 20, 20);
+}
