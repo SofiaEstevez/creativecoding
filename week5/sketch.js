a73b7a700719b65bf335daafc992ad76f453f6d4
Sofia Estevez <sve6@miami.edu>
2018-09-23 19:58:54 -0400

    hw5

--- week5/sketch.js
+++ week5/sketch.js
@@ -1,16 +1,11 @@
 function setup() {
-  createCanvas(800, 400);
-  background(204);
-}
-
-function setup() {
   createCanvas(500, 150);
 background(204);
 }
 
 function draw()
 {
-if(mouseIsPressed && mouseX < 250)
+if(mouseIsPressed && mouseX > 90 && mouseX < 150 && mouseY > 30 && mouseY < 90)
  {
 fill(255, 0 ,0);
  }
@@ -21,7 +16,7 @@ fill(255);
 
 rect(90, 30, 60, 60);
 
-if(mouseIsPressed && mouseX > 250)
+if(mouseIsPressed && mouseX > 330 && mouseX < 390 && mouseY > 30 && mouseY < 90)
 {
 fill(0, 255, 0);
 }
